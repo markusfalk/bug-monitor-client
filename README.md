271a9896fa83dbc4293c640da96a417345dfa42a
Markus Falk <mail@markus-falk.com>
2017-05-22 18:09:39 +0200

    [TASK] transfere from bug-monitor

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# bug-monitor-client
-client JS
+# Bug Monitor Client
+
+ALPHA - NOT READY FOR PRODUCTION.
+
+## Minimum Setup
+```html
+<script src="../src/bug-monitor-client.js"></script>
+<script>
+  setBugMonitorClientConfigDefaultValue('bugMonitorUrl', 'http://192.168.178.37:5000');
+</script>
+```
+
+## Options
+
+| Property  | Default | Type | Description |
+| -------- | -------- | ---- | ----------- |
+| bugMonitorUrl | '' | String | URL the script is sending to |
+| customFields  | {} | String or Array | A custom field that will be sent with the request |
+| httpMethod | 'POST' | String | HTTP method |
+
+### How to use
+
+```javascript
+setBugMonitorClientConfigDefaultValue('property', 'value');
+```
+
+## Sent Data
+
+Data that will be sent to the collecting endpoint:
+
+```javascript
+{
+  'appCodeName': '',
+  'appVersion': '',
+  'column': '',
+  'customFields': {},
+  'innerHeight': 0,
+  'innerHeight': 0,
+  'language': '',
+  'line': '',
+  'message': '',
+  'platform': '',
+  'stack': '',
+  'url': '',
+  'userAgent': ''
+}
+```
+
+### Custom Fields
+
+Send your own data with custom fields:
+
+```javascript
+setBugMonitorClientConfigDefaultValue('customProperty', 'customValue');
+```
+
+## Browser Support
+
+to be defined!
---
+++ src/bug-monitor-client.js
@@ -0,0 +1,84 @@
+/**
+  Bug Monitor Client
+  @author: mail@markus-falk.com
+  @url: https://github.com/markusfalk/bug-monitor
+  @license AGPL-3.0
+*/
+
+(function(window, navigator, undefined) {
+
+  'strict mode';
+
+  window.bugMonitorClientConfigDefault = {
+    bugMonitorUrl: '',
+    customFields: {},
+    httpMethod: 'POST'
+  };
+
+  window.setBugMonitorClientConfigDefaultValue = function(property, value) {
+
+     if(window.bugMonitorClientConfigDefault.hasOwnProperty(property)) {
+       window.bugMonitorClientConfigDefault[property] = value;
+     } else {
+       window.bugMonitorClientConfigDefault.customFields[property] = value;
+     }
+
+  }
+
+  sendErrorToBugMonitor = function(config, errorObject) {
+
+    var payload = {};
+
+    payload.customFields = window.bugMonitorClientConfigDefault.customFields;
+
+    // error
+    payload.url = errorObject.url;
+    payload.column = errorObject.column;
+    payload.line = errorObject.line;
+    payload.message = errorObject.message;
+    payload.stack = errorObject.errorObject.stack;
+
+    // navigator
+    payload.appCodeName = navigator.appCodeName;
+    payload.appVersion = navigator.appVersion;
+    payload.language = navigator.language;
+    payload.userAgent = navigator.userAgent;
+    payload.userAgent= navigator.userAgent;
+    payload.userAgent= navigator.userAgent;
+
+    // window
+    payload.innerHeight = window.innerHeight;
+    payload.innerWidth = window.innerWidth;
+
+    var xhr = new XMLHttpRequest();
+    xhr.open(window.bugMonitorClientConfigDefault.httpMethod, window.bugMonitorClientConfigDefault.bugMonitorUrl, true);
+    xhr.setRequestHeader('Content-type', 'application/json');
+    xhr.send(JSON.stringify(payload));
+
+  }
+
+  window.onerror = function(message, url, lineNumber, columnNumber, errorObject) {
+
+    var string = message.toLowerCase();
+
+    if (string.indexOf('script error') > -1) {
+      console.info('Script Error: See Browser Console for Detail');
+    } else {
+
+      var error = {
+        'message': message,
+        'url': url,
+        'line': lineNumber,
+        'column': columnNumber,
+        'errorObject': errorObject
+      };
+
+      sendErrorToBugMonitor(window.bugMonitorClientConfigDefault, error);
+
+    }
+
+    return false
+  }
+
+
+}(window, navigator));
