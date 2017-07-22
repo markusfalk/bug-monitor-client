0778f72cbbab2123c43afbe5bd3e32b19a8d54ce
Markus Falk <mail@markus-falk.com>
2017-07-22 10:42:09 +0200

    [FEATURE] add umd for global setting refs #13

--- src/bug-monitor-client.js
+++ src/bug-monitor-client.js
@@ -5,180 +5,201 @@
   @license AGPL-3.0
 */
 
-(function(window, navigator) {
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['dependency'], function (dependency) {
+            return (root.returnExportsGlobal = factory(dependency));
+        });
+    } else if (typeof module === 'object' && module.exports) {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory(require('dependency'));
+    } else {
+        // Browser globals
+        root.returnExportsGlobal = factory(root.dependency);
+    }
+}(this, function (dependency) {
 
-  'strict mode';
+    console.log(this, window, navigator);
 
-  // reference global for better minification
-  var bugMonitorClientConfigDefault = window.bugMonitorClientConfigDefault;
+    'strict mode';
 
-  /**
-  * provide default values for known options
-  * @private
-  */
-  var _setupDefaults = function() {
-    // set default values
-    bugMonitorClientConfigDefault = {
-      bugMonitorUrl: '',
-      customFields: {},
-      disabled: false,
-      httpMethod: 'POST',
-      timeout: 2000,
-      verbose: true
-    };
-  }
+    // reference global for better minification
+    var bugMonitorClientConfigDefault = window.bugMonitorClientConfigDefault;
 
-  /**
-  * sets values to the global config object
-  * @private
-  */
-  var _setCustomOptions = function() {
-    window.setBugMonitorClientConfigDefaultValue = function(property, value) {
-      if(bugMonitorClientConfigDefault.hasOwnProperty(property)) {
-        bugMonitorClientConfigDefault[property] = value;
-      } else {
-        bugMonitorClientConfigDefault.customFields[property] = value;
+    /**
+    * provide default values for known options
+    * @private
+    */
+    var _setupDefaults = function() {
+      // set default values
+      bugMonitorClientConfigDefault = {
+        bugMonitorUrl: '',
+        customFields: {},
+        disabled: false,
+        httpMethod: 'POST',
+        timeout: 2000,
+        verbose: true
+      };
+    }
+
+    /**
+    * sets values to the global config object
+    * @private
+    */
+    var _setCustomOptions = function() {
+      window.setBugMonitorClientConfigDefaultValue = function(property, value) {
+        if(bugMonitorClientConfigDefault.hasOwnProperty(property)) {
+          bugMonitorClientConfigDefault[property] = value;
+        } else {
+          bugMonitorClientConfigDefault.customFields[property] = value;
+        }
       }
-    }
-  };
-
-  /**
-  * makes bug-monitor-client more or less verbose
-  * @private
-  */
-  var _log = function(type, message) {
-    if(type === 'info' && bugMonitorClientConfigDefault.verbose) {
-      console.info(message);
-    } else if (type === 'warn' && bugMonitorClientConfigDefault.verbose) {
-      console.warn(message);
-    } else if (type === 'error') {
-      console.error(message);
-    }
-  };
-
-  /**
-   * Creates payload and sends it to the endpoint
-   * @private
-   */
-  var _sendErrorToBugMonitor = function(config, error) {
-
-    var payload = {};
-
-    payload.customFields = bugMonitorClientConfigDefault.customFields;
-
-    // error
-    payload.column = error.column;
-    payload.line = error.line;
-    payload.message = error.message;
-    payload.url = error.url;
-
-    if(error.errorObject) {
-      payload.stack = error.errorObject.stack;
-    }
-
-    // navigator
-    payload.language = navigator.language;
-    payload.userAgent = navigator.userAgent;
-
-    // window
-    payload.innerHeight = window.innerHeight;
-    payload.innerWidth = window.innerWidth;
-
-    var xhr = new XMLHttpRequest();
-
-    xhr.open(bugMonitorClientConfigDefault.httpMethod, bugMonitorClientConfigDefault.bugMonitorUrl, true);
-
-    //options
-    xhr.timeout = bugMonitorClientConfigDefault.timeout;
-    xhr.setRequestHeader('Content-type', 'application/json');
-
-    xhr.onload = function (response) {
-
-      switch (response.currentTarget.status) {
-        case  404:
-          _log('warn', 'The bug-monitor-client could not find your back-end! (' + response.currentTarget.status + ': ' + response.currentTarget.statusText + ')');
-          break;
-        case 200:
-          _log('info', 'The bug-monitor-client successfully reported an error to ' + bugMonitorClientConfigDefault.bugMonitorUrl);
-          break;
-        //default:
-
-      }
-
     };
 
-    xhr.ontimeout = function (err) {
-      _log('warn', 'The bug-monitor back-end timed out. ' + err);
+    /**
+    * makes bug-monitor-client more or less verbose
+    * @private
+    */
+    var _log = function(type, message) {
+      if(type === 'info' && bugMonitorClientConfigDefault.verbose) {
+        console.info(message);
+      } else if (type === 'warn' && bugMonitorClientConfigDefault.verbose) {
+        console.warn(message);
+      } else if (type === 'error') {
+        console.error(message);
+      }
     };
 
-    xhr.send(JSON.stringify(payload));
+    /**
+     * Creates payload and sends it to the endpoint
+     * @private
+     */
+    var _sendErrorToBugMonitor = function(config, error) {
 
-  }
+      var payload = {};
 
-  /**
-   * Sets Listener-function called by the browser 'onerror'
-   * @private
-   */
-  var _setErrorListener = function() {
-    window.onerror = function(message, url, lineNumber, columnNumber, errorObject) {
+      payload.customFields = bugMonitorClientConfigDefault.customFields;
 
-      var string = message.toLowerCase();
+      // error
+      payload.column = error.column;
+      payload.line = error.line;
+      payload.message = error.message;
+      payload.url = error.url;
 
-      if (!string.indexOf('script error') > -1) {
-
-        var error = {
-          'message': message,
-          'url': url,
-          'line': lineNumber,
-          'column': columnNumber,
-          'errorObject': errorObject
-        };
-
-        _sendErrorToBugMonitor(bugMonitorClientConfigDefault, error);
-
+      if(error.errorObject) {
+        payload.stack = error.errorObject.stack;
       }
 
-      return false;
+      // navigator
+      payload.language = navigator.language;
+      payload.userAgent = navigator.userAgent;
+
+      // window
+      payload.innerHeight = window.innerHeight;
+      payload.innerWidth = window.innerWidth;
+
+      var xhr = new XMLHttpRequest();
+
+      xhr.open(bugMonitorClientConfigDefault.httpMethod, bugMonitorClientConfigDefault.bugMonitorUrl, true);
+
+      //options
+      xhr.timeout = bugMonitorClientConfigDefault.timeout;
+      xhr.setRequestHeader('Content-type', 'application/json');
+
+      xhr.onload = function (response) {
+
+        switch (response.currentTarget.status) {
+          case  404:
+            _log('warn', 'The bug-monitor-client could not find your back-end! (' + response.currentTarget.status + ': ' + response.currentTarget.statusText + ')');
+            break;
+          case 200:
+            _log('info', 'The bug-monitor-client successfully reported an error to ' + bugMonitorClientConfigDefault.bugMonitorUrl);
+            break;
+          //default:
+
+        }
+
+      };
+
+      xhr.ontimeout = function (err) {
+        _log('warn', 'The bug-monitor back-end timed out. ' + err);
+      };
+
+      xhr.send(JSON.stringify(payload));
+
     }
-  };
 
-  /**
-   * Go thru set options and validate minimum setup
-   * @private
-   */
+    /**
+     * Sets Listener-function called by the browser 'onerror'
+     * @private
+     */
+    var _setErrorListener = function() {
+      window.onerror = function(message, url, lineNumber, columnNumber, errorObject) {
 
-  var _validateSetup = function() {
+        var string = message.toLowerCase();
 
-    var isValid = true;
+        if (!string.indexOf('script error') > -1) {
 
-    try {
-      if(bugMonitorClientConfigDefault.bugMonitorUrl === '') {
-        throw new Error(
-          'No url set for bug-monitor-client! Add bugMonitorUrl option to your setup: ' +
-          'setBugMonitorClientConfigDefaultValue(\'bugMonitorUrl\', \'http:\/\/your-url.com\');'
-        );
+          var error = {
+            'message': message,
+            'url': url,
+            'line': lineNumber,
+            'column': columnNumber,
+            'errorObject': errorObject
+          };
+
+          _sendErrorToBugMonitor(bugMonitorClientConfigDefault, error);
+
+        }
+
+        return false;
       }
-    } catch (err) {
-      _log('error', err);
+    };
+
+    /**
+     * Go thru set options and validate minimum setup
+     * @private
+     */
+
+    var _validateSetup = function() {
+
+      var isValid = true;
+
+      try {
+        if(bugMonitorClientConfigDefault.bugMonitorUrl === '') {
+          throw new Error(
+            'No url set for bug-monitor-client! Add bugMonitorUrl option to your setup: ' +
+            'setBugMonitorClientConfigDefaultValue(\'bugMonitorUrl\', \'http:\/\/your-url.com\');'
+          );
+        }
+      } catch (err) {
+        _log('error', err);
+      }
+
+      if (bugMonitorClientConfigDefault.disabled) {
+        _log('info', 'The bug-monitor-client script has been disabled on this page.');
+        isValid = false;
+      }
+
+      return isValid;
+
     }
 
-    if (bugMonitorClientConfigDefault.disabled) {
-      _log('info', 'The bug-monitor-client script has been disabled on this page.');
-      isValid = false;
-    }
+    // init the whole thing
+    _setupDefaults();
+    _setCustomOptions();
 
-    return isValid;
+    document.addEventListener('DOMContentLoaded', function() {
+      if(_validateSetup()) {
+        _setErrorListener();
+      }
+    });
 
-  }
-
-  // init the whole thing
-  _setupDefaults();
-  _setCustomOptions();
-
-  document.addEventListener('DOMContentLoaded', function() {
-    if(_validateSetup()) {
-      _setErrorListener();
-    }
-  });
-
-}(window, navigator));
+    // Just return a value to define the module export.
+    // This example returns an object, but the module
+    // can return a function as the exported value.
+    return {};
+}));
