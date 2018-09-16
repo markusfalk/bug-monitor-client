4182d0c08319d4b4c48a72ac7337413891c65da5
Markus Falk <mail@markus-falk.com>
2018-09-16 10:24:21 +0200

    [TASK] remove dist from git

--- dist/custom-field.interface.d.ts
+++
@@ -1,4 +0,0 @@
-export interface CustomField {
-    [name: string]: string | [] | boolean | object;
-}
-//# sourceMappingURL=custom-field.interface.d.ts.map
--- dist/custom-fields.interface.d.ts
+++
@@ -1,4 +0,0 @@
-export interface CustomField {
-    [name: string]: string | [] | boolean | object;
-}
-//# sourceMappingURL=custom-fields.interface.d.ts.map
