20eabfd5bc999fbd27923e44bd6e1fcc786c96ec
Markus Falk <mail@markus-falk.com>
2018-11-07 20:51:42 +0100

    [TASK] add cypress testing

---
+++ cypress/integration/bmc.spec.ts
@@ -0,0 +1,9 @@
+describe('Bug Monitor Client', () => {
+  it('Visits Angular 6', () => {
+    // cy.server();
+    // cy.route('**/errors').as('Errors');
+    cy.visit('http://localhost:4200/');
+    // cy.get('#angularError').click();
+    // cy.wait('@Errors');
+  });
+});
